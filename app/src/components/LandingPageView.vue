de9a0a3ae61f2769bb3e2da666dedaac490ff174
samuraikun <tazitaziawawa@gmail.com>
2016-12-09 00:13:22 +0900

    setting app

---
+++ app/src/components/LandingPageView.vue
@@ -0,0 +1,31 @@
+<style lang="scss" scoped>
+  img {
+    margin-top: -25px;
+    width: 450px;
+  }
+</style>
+
+<template>
+  <div>
+    <img src="./LandingPageView/assets/logo.png" alt="electron-vue">
+    <h1>Vue Electron</h1>
+    <current-page></current-page>
+    <versions></versions>
+    <links></links>
+  </div>
+</template>
+
+<script>
+  import CurrentPage from './LandingPageView/CurrentPage'
+  import Links from './LandingPageView/Links'
+  import Versions from './LandingPageView/Versions'
+
+  export default {
+    components: {
+      CurrentPage,
+      Links,
+      Versions
+    },
+    name: 'landing-page'
+  }
+</script>
